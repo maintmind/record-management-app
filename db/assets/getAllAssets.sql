6a58e6964f7fbc0651d1a678a04ac08008c38612
Camille <shawn.koga@aggiemail.usu.edu>
2017-10-16 16:55:45 -0600

    added endpoints

---
+++ db/assets/getAllAssets.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM assets
+WHERE user_id = $1;
--- server/server.js
+++ server/server.js
@@ -1,20 +1,44 @@
+require('dotenv').config();
+
 const bodyParser = require('body-parser'),
         cors = require('cors'),
         massive = require('massive'),
         express = require('express');
 
-
 const app = express();
+const controller = require('./controller');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+//DATABASE CONNECTION
+massive(process.env.CONNECTIONSTRING).then(db => {
+        app.set('db', db)
+})
+
+//AUTHENTICATION (WHEN WE ARE READY)
 
 
+//ASSETS ENDPOINTS
+app.get('/api/assets/get_all/:id', controller.getAllAssets)
+app.post('/api/assets/add', controller.addAsset)
+
+//CATEGORY ENDPOINTS
+app.get('/api/categories/get_all/:id', controller.getAllCategories)
+app.post('/api/categories/add', controller.addCategory)
+
+//LOGS ENDPOINTS
+
+
+//REMINDERS ENDPOINTS
 
 
 
 app.get('*', (req, res) => {
         console.log("None Met");
         res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-    })
-    
+})
+
 const port = 3005
 app.listen(port, console.log(`Listening on ${port}`))
 
