69e7e4f4d0f0cf27c90977b7918645b50e116a26
James Carlson <carlson.james@gmail.com>
2017-10-17 17:10:54 -0600

    non-styled user input forms connected to redux

--- src/router.js
+++ src/router.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Dashboard from './components/Dashboard/Dashboard';
 import Landing from './components/Landing/Landing';
+import UserInputForm from './components/UserInputForm/UserInputForm'
 
 
 export default (
     <Switch>
         <Route exact path ="/" component={Landing} />
         <Route path ="/dashboard" component={Dashboard} />
+        <Route path ="/form" component={UserInputForm} />
     </Switch>
 )
