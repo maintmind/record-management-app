26fbc1149a0aeb26a2d77551296da7cdcb8d10e0
marachi2 <marissaf32@gmail.com>
2017-10-17 16:41:59 -0600

    reducer connected, broke out asset component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,58 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import Assets from './../Assets/Assets';
 import Footer from './../Footer/Footer';
+
 import './Dashboard.css';
 
 export default class Dashboard extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            assetsToDisplay: [],
-            showAssetAdder: false
-        }
-    }
-
-    componentDidMount() {
-        axios.get(`/api/assets/get_all/1`).then(res => {
-            console.log(res.data)
-            this.setState({
-                assetsToDisplay: res.data
-            })
-        })
-    }
-
-    openAssetModal() {
-        this.setState({
-            showAssetAdder: !this.state.showAssetAdder
-        })
-        console.log(this.state.showAssetAdder)
-    }
-
-    assetRotate(num) {
-        console.log(num)
-    }
-
     render() {
-        const displayAsset = this.state.assetsToDisplay.map((c, i) => {
-            return (
-                <button key={i} className="asset_tab" onClick={() => this.assetRotate(c.asset_id)}>
-                    {c.title}
-                </button>
-            )
-        })
-
         return (
             <div className="dash_main_container">
                 <h2>- Header</h2>
-                <div className="asset_tabs_container">
-                    {displayAsset}
-                    <button onClick={() => { this.openAssetModal() }} className="add_asset_button">ADD ASSET</button>
-                </div>
-
-
-                <p>- Category list</p>
-                <p>- Log dropdown</p>
+                <Assets />
                 <Footer />
             </div>
         );
