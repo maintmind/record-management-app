b3f575b944f33f02e50713a8a93bf762df3242b0
Camille <shawn.koga@aggiemail.usu.edu>
2017-10-18 15:12:56 -0600

    fixed functionality on log display and fixed the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Footer.css';
-import {Link} from 'react-router-dom';
 
-export default class Footer extends Component{
-render(){
-    
-    return(
-      <div className="big-container">  
-        <div className="footer-container">
-            <div className="logo">
-                <h4>Maintenance<span>Minder</span></h4>
-            </div>
-      </div>
-    </div>
-
-
-
-
-    )
-}
+export default class Footer extends Component {
+    render() {
+        return (
+                <div className="footer-container">
+                    <div className="logo">
+                        <h4>Maintenance<span>Minder</span></h4>
+                    </div>
+                </div>
+        )
+    }
 
 }
